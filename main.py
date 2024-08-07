b8331d0ce260bbc7a1b2eee5dcf40e08477ffce1
Alize21 <pusikentut@gmail.com>
2024-08-07 21:19:51 +0700

    membuat paddle bergerak

--- main.py
+++ main.py
@@ -22,11 +22,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        paddle_1.direction = "up"
+    if keys[pygame.K_s]:
+        paddle_1.direction = "down"
+    if keys[pygame.K_o]:
+        paddle_2.direction = "up"
+    if keys[pygame.K_l]:
+        paddle_2.direction = "down"
+
+    paddle_1.set_direction()
+    paddle_2.set_direction()
 
     paddle_1.draw_paddle(screen)
     paddle_2.draw_paddle(screen)
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(60)
 
 pygame.quit()
--- paddle.py
+++ paddle.py
@@ -5,6 +5,16 @@ class Paddle:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        self.direction = None
+        self.speed = 10
+
+    def set_direction(self):
+        if self.direction == "up":
+            self.y -= self.speed
+            self.direction = None
+        if self.direction == "down":
+            self.y += self.speed
+            self.direction = None
 
     def draw_paddle(self,screen):
         pygame.draw.rect(screen, "white", (self.x, self.y, 15, 130))
